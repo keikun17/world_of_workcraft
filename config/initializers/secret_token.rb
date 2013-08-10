acde5da630b792a1eaf9fafc0b49666f1822c60a
Buddy Magsipoc <keikun17@gmail.com>
2013-08-10 13:54:14 +0800

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldOfWorkcraft::Application.config.secret_key_base = '29fb18d81acc8071b8b18460465f39c0fc1fb52f92a03f074a3a34936850d4d764f13835dc5b12d2d13a549aad388b269c3cf260807329a1707c09aca5fde070'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WorldOfWorkcraft::Application.config.session_store :cookie_store, key: '_world_of_workcraft_session'
